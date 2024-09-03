a9ab600e3a3bce378f8b979e3a4073bf8fd3035e
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-03 22:30:47 +0300

    Немного доработал задание 1

--- Task01.py
+++ Task01.py
@@ -1,3 +1,13 @@
 from Task01.Student import Student
 
-s = Student("Иванов Иван Иванович")
+
+student = Student("Иван Иванов", "subject.csv")
+student.add_grade("Математика", 4)
+# student.add_test_score("Математика", 85)
+student.add_grade("История", 5)
+# student.add_test_score("История", 92)
+average_grade = student.get_average_grade()
+print(f"Средний балл: {average_grade}")
+# average_test_score = student.get_average_test_score("Математика")
+# print(f"Средний результат по тестам по математике: {average_test_score}")
+print(student)
--- Task01/Student.py
+++ Task01/Student.py
@@ -1,16 +1,43 @@
 from Task01.FIO import FIO
 from Task01.Score import Score
-from Task01.Subject import Subject
+import csv
 
 
 class Student:
     name = FIO()
-    test_score = Score(0, 100, f"Результат теста должен быть целым числом от 0 до 100")
-    subject_score = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
-    subject = Subject("./Task01/subject.csv")
+    # test_score = Score(0, 100, f"Результат теста должен быть целым числом от 0 до 100")
+    # subject_score = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
 
-    def __init__(self, name):
+    def __init__(self, name, subjects_file):
         self.name = name
-        self.subjects = {}
+        self.subjects = self.load_subjects(subjects_file)
 
+    def __str__(self):
+        result = f"Студент: {self.name}\n\r"
+        result += f"Предметы: {', '.join(self.subjects.keys())}"
+        return result
+
+    def add_grade(self, subject: str, grade: int):
+        g = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
+        g = grade
+        if subject in self.subjects.keys():
+            self.subjects[subject]["grade"].append(g)
+
+    def get_average_grade(self) -> float:
+        grades = []
+        for grades_list in self.subjects.values():
+            grades.extend(grades_list["grade"])
+        return sum(grades) / len(grades)
+
+
+
+
+    @staticmethod
+    def load_subjects(subjects_file) -> dict:
+        result = dict()
+        with open(subjects_file, 'r', newline='', encoding="utf-8") as f:
+            fr = csv.reader(f)
+            for sb in list(fr)[0]:
+                result[sb] = {"grade": [], "test_score": []}
+        return result
 
