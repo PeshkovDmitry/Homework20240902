12d290b6b833c158e3993ce9340f4184c905c1c1
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-04 20:55:44 +0300

    Выполнено задание 1

--- Task01/Student.py
+++ Task01/Student.py
@@ -5,23 +5,26 @@ import csv
 
 class Student:
     name = FIO()
-    # test_score = Score(0, 100, f"Результат теста должен быть целым числом от 0 до 100")
-    # subject_score = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
+    current_grade = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
+    current_score = Score(0, 100, f"Результат теста должен быть целым числом от 0 до 100")
 
     def __init__(self, name, subjects_file):
         self.name = name
         self.subjects = self.load_subjects(subjects_file)
 
     def __str__(self):
+        used_subjects = []
+        for subject in self.subjects.keys():
+            if len(self.subjects[subject]["grade"]) or len(self.subjects[subject]["test_score"]):
+                used_subjects.append(subject)
         result = f"Студент: {self.name}\n\r"
-        result += f"Предметы: {', '.join(self.subjects.keys())}"
+        result += f"Предметы: {', '.join(used_subjects)}"
         return result
 
     def add_grade(self, subject: str, grade: int):
-        g = Score(2, 5, f"Оценка должна быть целым числом от 2 до 5")
-        g = grade
+        self.current_grade = grade
         if subject in self.subjects.keys():
-            self.subjects[subject]["grade"].append(g)
+            self.subjects[subject]["grade"].append(self.current_grade)
 
     def get_average_grade(self) -> float:
         grades = []
@@ -29,7 +32,14 @@ class Student:
             grades.extend(grades_list["grade"])
         return sum(grades) / len(grades)
 
+    def add_test_score(self, subject, test_score):
+        self.current_score = test_score
+        if subject in self.subjects.keys():
+            self.subjects[subject]["test_score"].append(self.current_score)
 
+    def get_average_test_score(self, subject):
+        scores = self.subjects[subject]["test_score"]
+        return sum(scores) / len(scores)
 
 
     @staticmethod
